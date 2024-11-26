fbd85d4b963eea1fd7426f32a0835dad7bf3e8b9
Mikel <mikelozit@hotmail.com>
2024-11-26 17:20:01 +0000

    Meh

--- gui.py
+++ gui.py
@@ -2,9 +2,15 @@ import functions
 import FreeSimpleGUI as Fsg
 
 label = Fsg.Text("Type in a to-do")
-input_box = Fsg.InputText(tooltip="Enter todo")
+input_box = Fsg.InputText(tooltip="Enter todo", key='todo')
 add_button = Fsg.Button("Add")
 
-window = Fsg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = Fsg.Window('My To-Do App',
+                    layout=[[label], [input_box, add_button]],
+                    font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+
+
 window.close()
