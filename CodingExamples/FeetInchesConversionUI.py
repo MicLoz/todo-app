7965079405eedabd96988a3a07e8f4739c65bf78
Mikel <mikelozit@hotmail.com>
2024-12-15 21:56:26 +0000

    Added exit button and changed theme to black for Feet and Inches converter

--- CodingExamples/FeetInchesConversionUI.py
+++ CodingExamples/FeetInchesConversionUI.py
@@ -1,15 +1,18 @@
 import FreeSimpleGUI as Fsg
 
+Fsg.theme("Black")
+
 feet_text = Fsg.Text("Enter feet:")
 inch_text = Fsg.Text("Enter inches:")
 ft_input = Fsg.Input(key="feet_entered")
 in_input = Fsg.Input(key="inches_entered")
 convert_button = Fsg.Button("Convert", key="convert_button_pressed")
 meters_text = Fsg.Text("Meters: ", key="meters_text_key")
+exit_button = Fsg.Button("Exit")
 
 window = Fsg.Window("Convertor", layout=[[feet_text, ft_input],
                                                 [inch_text, in_input],
-                                                [convert_button, meters_text]
+                                                [convert_button, meters_text, exit_button]
                                              ])
 
 while True:
@@ -21,6 +24,8 @@ while True:
             meters = round((feet * 0.3048) + (inches * 0.0254), 2)
             new_meters_text = "Meters: " + str(meters)
             window["meters_text_key"].update(value=new_meters_text)
+        case "Exit":
+            break
         case Fsg.WIN_CLOSED:
             break
 window.close()
