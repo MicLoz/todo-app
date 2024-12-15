f34c7561814f0fe7d8ded7ba7488bc098a9e6752
Mikel <mikelozit@hotmail.com>
2024-12-15 20:52:12 +0000

    Implement Complete and Exit buttons. Complete Feet and Inches conversion program

--- CodingExamples/FeetInchesConversionUI.py
+++ CodingExamples/FeetInchesConversionUI.py
@@ -2,10 +2,25 @@ import FreeSimpleGUI as Fsg
 
 feet_text = Fsg.Text("Enter feet:")
 inch_text = Fsg.Text("Enter inches:")
-ft_input = Fsg.Input()
-in_input = Fsg.Input()
-convert_button = Fsg.Button("Convert")
+ft_input = Fsg.Input(key="feet_entered")
+in_input = Fsg.Input(key="inches_entered")
+convert_button = Fsg.Button("Convert", key="convert_button_pressed")
+meters_text = Fsg.Text("Meters: ", key="meters_text_key")
 
-window = Fsg.Window("Convertor", layout=[[feet_text, ft_input], [inch_text, in_input], [convert_button]])
-window.read()
+window = Fsg.Window("Convertor", layout=[[feet_text, ft_input],
+                                                [inch_text, in_input],
+                                                [convert_button, meters_text]
+                                             ])
+
+while True:
+    event, value = window.read()
+    match event:
+        case "convert_button_pressed":
+            feet = float(window["feet_entered"].get())
+            inches = float(window["inches_entered"].get())
+            meters = round((feet * 0.3048) + (inches * 0.0254), 2)
+            new_meters_text = "Meters: " + str(meters)
+            window["meters_text_key"].update(value=new_meters_text)
+        case Fsg.WIN_CLOSED:
+            break
 window.close()
--- gui.py
+++ gui.py
@@ -5,12 +5,17 @@ label = Fsg.Text("Type in a to-do")
 input_box = Fsg.InputText(tooltip="Enter todo", key='todo_iText')
 add_button = Fsg.Button("Add")
 list_box = Fsg.Listbox(values=functions.get_todos(), key='todo_list',
-                       enable_events=True, size=[45, 10])
+                       enable_events=True, size=(45, 10))
 edit_button = Fsg.Button("Edit")
+complete_button = Fsg.Button("Complete")
+exit_button = Fsg.Button("Exit")
 
 
 window = Fsg.Window('My To-Do App',
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -39,8 +44,16 @@ while True:
                 Fsg.Popup("Select Item from list before you Edit.")
         case "todo_list":
                 window["todo_iText"].update(value=values['todo_list'][0])
-
+        case "Complete":
+            todo_to_complete = values['todo_list'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todo_list'].update(values=todos)
+            window["todo_iText"].update(value="")
         case Fsg.WIN_CLOSED:
+            break
+        case "Exit":
             break
 
 window.close()
